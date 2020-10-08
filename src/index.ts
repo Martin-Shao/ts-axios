365c8976baff1145b06dc770518814327cc88e5b
Martin-Shao <shaogucheng@hotmail.com>
2020-10-08 14:09:26 +0800

    axios的Promise化

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import { AxiosRequestConfig } from './types'
+import { AxiosRequestConfig, AxiosPromise } from './types'
 import { buildURL } from './helpers/url'
 import { transformRequest } from './helpers/data'
 import { processHeaders } from './helpers/headers'
 import xhr from './xhr'
 
-function axios(config: AxiosRequestConfig): void {
+function axios(config: AxiosRequestConfig): AxiosPromise {
   // TODO
   processConfig(config)
-  xhr(config)
+  return xhr(config)
 }
 
 function processConfig(config: AxiosRequestConfig): void {

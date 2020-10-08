1994a8f4e2034269429d46921bd307b006df63fe
Martin-Shao <shaogucheng@hotmail.com>
2020-10-08 23:39:27 +0800

    feature: 配置合并

--- src/core/Axios.ts
+++ src/core/Axios.ts
@@ -1,7 +1,7 @@
-import { resolve } from "dns";
 import { AxiosPromise, AxiosRequestConfig, AxiosResponse, Method, RejectedFn, ResolvedFn } from "../types";
 import dispatchRequest from './dispatchRequest'
 import InterceptorManage from "./InterceptorManage";
+import mergeConfig from "./mergeConfig";
 
 interface Interceptors {
   request: InterceptorManage<AxiosRequestConfig>;
@@ -34,6 +34,8 @@ export default class Axios {
     } else {
       config = url
     }
+
+    config = mergeConfig(this.defaults, config)
 
     const chain: PromiseChain<any>[] = [{
       resolved: dispatchRequest,

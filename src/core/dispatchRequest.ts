0fb8613e79bb621c20a2298766527d6945131192
Martin-Shao <shaogucheng@hotmail.com>
2020-10-09 11:08:11 +0800

    refactor: 请求和响应transform逻辑的重构

--- src/core/dispatchRequest.ts
+++ src/core/dispatchRequest.ts
@@ -1,21 +1,20 @@
 import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from '../types'
+import { flattenHeaders } from '../helpers/headers'
 import { buildURL } from '../helpers/url'
-import { transformRequest, transformResponse } from '../helpers/data'
-import { flattenHeaders, processHeaders } from '../helpers/headers'
+import transform from './transform'
 import xhr from './xhr'
 
 export default function dispatchRequest(config: AxiosRequestConfig): AxiosPromise {
   // TODO
   processConfig(config)
-  return xhr(config).then((res) => {
+  return xhr(config).then(res => {
     return transformResponseData(res)
   })
 }
 
 function processConfig(config: AxiosRequestConfig): void {
   config.url = transformURL(config)
-  config.headers = transformHeaders(config)
-  config.data = transformRequestData(config)
+  config.data = transform(config.data, config.headers, config.transformRequest)
   config.headers = flattenHeaders(config.headers, config.method!)
 }
 
@@ -24,16 +23,7 @@ function transformURL(config: AxiosRequestConfig): string {
   return buildURL(url!, params)
 }
 
-function transformRequestData(config: AxiosRequestConfig): any {
-  return transformRequest(config.data)
-}
-
-function transformHeaders(config: AxiosRequestConfig): any {
-  const { headers = {}, data } = config
-  return processHeaders(headers, data)
-}
-
 function transformResponseData(res: AxiosResponse): AxiosResponse {
-  res.data = transformResponse(res.data)
+  res.data = transform(res.data, res.headers, res.config.transformResponse)
   return res
 }

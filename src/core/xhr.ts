9fa859f0d5db6ce211437720be4f063a216b5914
Martin-Shao <shaogucheng@hotmail.com>
2020-10-09 15:22:02 +0800

    refactor(xhr核心模块重构): xhr核心模块代码重构

--- src/core/xhr.ts
+++ src/core/xhr.ts
@@ -24,93 +24,111 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
 
     const request = new XMLHttpRequest()
 
-    if (responseType) {
-      request.responseType = responseType
-    }
-
-    if (timeout) {
-      request.timeout = timeout
-    }
-
-    // 创建一个跨站点访问控制（cross-site Access-Control）请求
-    if (withCredentials) {
-      request.withCredentials = withCredentials
-    }
-
     request.open(method.toUpperCase(), url!, true)
 
-    request.onreadystatechange = function handleLoad() {
-      if (request.readyState !== 4) {
-        return
-      }
+    configureRequest()
 
-      if (request.status === 0) {
-        return
-      }
+    addEvents()
 
-      const responseHeaders = parseHeaders(request.getAllResponseHeaders())
-      const responseData = responseType !== 'text' ? request.response : request.responseText
-      const response: AxiosResponse = {
-        data: responseData,
-        status: request.status,
-        statusText: request.statusText,
-        headers: responseHeaders,
-        config,
-        request
-      }
-      handleResponse(response)
-    }
+    processHeaders()
 
-    // 处理网络错误
-    request.onerror = function handleError() {
-      reject(createError('Network Error', config, null, request))
-    }
-
-    // 处理请求超时错误
-    request.ontimeout = function handleTimeout() {
-      reject(createError(`Timeout of ${timeout} ms exceeded`, config, 'ECONNABORTED', request))
-    }
-
-    if (onDownloadProgress) {
-      request.onprogress = onDownloadProgress
-    }
-
-    if (onUploadProgress) {
-      request.upload.onprogress = onUploadProgress
-    }
-
-    if (isFormData(data)) {
-      delete headers['Content-Type']
-    }
-
-    if ((withCredentials || isURLSameOrigin(url!)) && xsrfCookieName) {
-      const xsrfValue = cookie.read(xsrfCookieName)
-      if (xsrfValue && xsrfHeaderName) {
-        headers[xsrfHeaderName] = xsrfValue
-      }
-    }
-
-    Object.keys(headers).forEach(name => {
-      if (data === null && name.toLowerCase() === 'content-type') {
-        delete headers[name]
-      } else {
-        request.setRequestHeader(name, headers[name])
-      }
-    })
-
-    // 添加取消请求的逻辑
-    if (cancelToken) {
-      cancelToken.promise
-        .then(reason => {
-          request.abort()
-          reject(reason)
-        })
-        .catch(error => {
-          reject(error)
-        })
-    }
+    processCancel()
 
     request.send(data)
+
+    // 处理配置相关逻辑
+    function configureRequest(): void {
+      if (responseType) {
+        request.responseType = responseType
+      }
+
+      // 解决超时问题
+      if (timeout) {
+        request.timeout = timeout
+      }
+
+      // 创建一个跨站点访问控制（cross-site Access-Control）请求
+      if (withCredentials) {
+        request.withCredentials = withCredentials
+      }
+    }
+
+    // 处理事件相关逻辑
+    function addEvents(): void {
+      request.onreadystatechange = function handleLoad() {
+        if (request.readyState !== 4) {
+          return
+        }
+
+        if (request.status === 0) {
+          return
+        }
+
+        const responseHeaders = parseHeaders(request.getAllResponseHeaders())
+        const responseData = responseType !== 'text' ? request.response : request.responseText
+        const response: AxiosResponse = {
+          data: responseData,
+          status: request.status,
+          statusText: request.statusText,
+          headers: responseHeaders,
+          config,
+          request
+        }
+        handleResponse(response)
+      }
+      // 处理网络错误
+      request.onerror = function handleError() {
+        reject(createError('Network Error', config, null, request))
+      }
+
+      // 处理请求超时错误
+      request.ontimeout = function handleTimeout() {
+        reject(createError(`Timeout of ${timeout} ms exceeded`, config, 'ECONNABORTED', request))
+      }
+
+      if (onDownloadProgress) {
+        request.onprogress = onDownloadProgress
+      }
+
+      if (onUploadProgress) {
+        request.upload.onprogress = onUploadProgress
+      }
+    }
+
+    function processHeaders(): void {
+      if (isFormData(data)) {
+        delete headers['Content-Type']
+      }
+
+      if ((withCredentials || isURLSameOrigin(url!)) && xsrfCookieName) {
+        const xsrfValue = cookie.read(xsrfCookieName)
+        if (xsrfValue && xsrfHeaderName) {
+          headers[xsrfHeaderName] = xsrfValue
+        }
+      }
+
+      Object.keys(headers).forEach(name => {
+        if (data === null && name.toLowerCase() === 'content-type') {
+          delete headers[name]
+        } else {
+          request.setRequestHeader(name, headers[name])
+        }
+      })
+    }
+
+    function processCancel(): void {
+      // 添加取消请求的逻辑
+      if (cancelToken) {
+        cancelToken.promise
+          .then(reason => {
+            request.abort()
+            reject(reason)
+          })
+          .catch(error => {
+            reject(error)
+          })
+      }
+    }
 
     // 处理请求 200 ~ 300 之间请求错误
     function handleResponse(response: AxiosResponse): void {
